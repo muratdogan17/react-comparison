ac43cec4544fd56377012e087f702a2d97835a15
Murat Dogan <muratdogan@yahoo.com>
2020-06-01 23:08:15 +0300

    minor enhancements and prettier implemented

--- src/Comparison.jsx
+++ src/Comparison.jsx
@@ -1,34 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 const propTypes = {
-    styles: PropTypes.object
-}
+    styles: PropTypes.object,
+    side1: PropTypes.node.isRequired,
+    side2: PropTypes.node.isRequired,
+};
 
 const defaultProps = {
     styles: {
         wrapper: {
-            position: "relative"
+            position: "relative",
         },
         side1: {},
         side2: {
             position: "absolute",
-            left:0,
-            top:0
-        }
-    }
-}
+            left: 0,
+            top: 0,
+        },
+    },
+};
 
-const Comparison = () => {
-    const {styles} = this.props || {};
-
+const Comparison = ({ side1, side2, styles = {} }) => {
     return (
         <div style={styles.wrapper}>
-            <div style={styles.side1}>comparison 1</div>
-            <div style={styles.side2}>comparison 2</div>
+            <div style={styles.side1}>{side1}</div>
+            <div style={styles.side2}>{side2}</div>
         </div>
     );
-}
+};
 
 Comparison.propTypes = propTypes;
 Comparison.defaultProps = defaultProps;
